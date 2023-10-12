24f18dd8db1615239b0a34495bd6067883462184
Dulabuddi <147624021+Dulabuddi@users.noreply.github.com>
2023-10-12 23:44:57 +0530

    Create buddhi input 4.py

---
+++ buddhi input 4.py
@@ -0,0 +1,12 @@
+# Define for list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Initialize a variable to store the sum
+sum_of_numbers = 0
+
+# Use a for loop to iterate through the list
+for num in numbers:
+    sum_of_numbers += num
+
+# Print the result
+print(f"The sum of numbers is: {sum_of_numbers}")
