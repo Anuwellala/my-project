6c864072617c134ff6d29c4175cd0c5afcb60ad9
sandunma <147874385+sandunma@users.noreply.github.com>
2023-10-16 01:48:14 +0530

    Create mobile2.py

---
+++ mobile2.py
@@ -0,0 +1,8 @@
+# Sample list of numbers in file
+numbers = [5, 2, 8, 1, 9]
+
+# Sort the list in ascending orders
+sorted_numbers = sorted(numbers)
+
+# Print the sorted list
+print(sorted_numbers)
