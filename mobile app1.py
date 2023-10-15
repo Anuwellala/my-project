0426654d969ce0b319499f17f839a4842e02fe95
sandunma <147874385+sandunma@users.noreply.github.com>
2023-10-16 01:46:35 +0530

    Create mobile app1.py

---
+++ mobile app1.py
@@ -0,0 +1,8 @@
+#The  Sample list of string
+data = ["Apple", "Banana", "Cherry", "Date", "Fig"]
+
+# Sort the list in alphabetical order
+sorted_data = sorted(data)
+
+# Print the sorted list
+print(sorted_data)
